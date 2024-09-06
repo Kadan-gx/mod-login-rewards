73845ee1d2c85f303a136d3c86fb7ea3cc1460e5
Gx-Harbinger <draftmail50@gmail.com>
2024-09-06 22:22:47 +0200

    fix build

--- src/login_rewards_loader.cpp
+++ src/login_rewards_loader.cpp
@@ -8,7 +8,7 @@ void AddMyLoginRewards();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addmod_LoginRewardsScripts()
+void Addlogin_rewardsScripts()
 {
     AddMyLoginRewards();
 }
