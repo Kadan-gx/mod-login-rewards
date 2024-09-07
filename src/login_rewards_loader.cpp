eaa06e981fb4c04196933039d177fc0eaf151c86
Gx-Harbinger <draftmail50@gmail.com>
2024-09-07 09:28:21 +0200

    fix makelist script

--- src/login_rewards_loader.cpp
+++ src/login_rewards_loader.cpp
@@ -8,7 +8,7 @@ void AddMyLoginRewards();
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addlogin_rewardsScripts()
+void Addmod_login_rewardsScripts()
 {
     AddMyLoginRewards();
 }
