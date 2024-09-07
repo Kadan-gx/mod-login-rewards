48c39ea6428dd0470a323b8f021f3033542d026c
Gx-Harbinger <draftmail50@gmail.com>
2024-09-07 10:46:46 +0200

    updated sql files

--- data/sql/db-characters/base/login_rewards_characters.sql
+++ data/sql/db-characters/base/login_rewards_characters.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS `login_rewards`;
-CREATE TABLE `acore_characters`.`login_rewards` (
+CREATE TABLE IF NOT EXISTS `login_rewards` (
   `guid` INT NOT NULL,
   `lastClaimed` INT NULL,
   `reward` TINYINT NULL,
--- data/sql/db-world/base/login_rewards_data.sql
+++ data/sql/db-world/base/login_rewards_data.sql
@@ -1,6 +1,12 @@
-DROP TABLE IF EXISTS `acore_world`.`login_rewards_data`;
-CREATE TABLE `acore_world`.`login_rewards_data` (
+CREATE TABLE IF NOT EXISTS `login_rewards_data` (
   `day` TINYINT NOT NULL,
   `item` INT NULL,
   `count` TINYINT NULL,
-  PRIMARY KEY (`day`));
+PRIMARY KEY (`day`));
+
+INSERT INTO login_rewards_data (id, account_id, reward_id) VALUES 
+(0, 1191, 1),
+(1, 1191, 1),
+(2, 1191, 1),
+(3, 1191, 1),
+(4, 1191, 1);
